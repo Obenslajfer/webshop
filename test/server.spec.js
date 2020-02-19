b4e05049bc46bd230772f895bb201c6ada195748
Pontus Bovin <pontus@pontusbovin.com>
2020-02-19 10:36:54 +0100

    supertest initzialiation

---
+++ test/server.spec.js
@@ -0,0 +1,21 @@
+// test för server.js
+const supertest = require("supertest")
+const {app, port} = require("../src/server")
+
+
+
+describe("simpe test, doesnt work", ()=>{
+    let server
+
+    beforeEach(() => {
+         server = app.listen(port,() => console.log("testing server up"))
+     })
+
+    it("should respond to /",(done)=>{
+       supertest(server).get("/").expect(200, done);
+
+    })
+    afterEach((done)=>{
+        server.close(done);
+    })
+})
